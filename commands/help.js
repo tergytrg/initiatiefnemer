b7362d54a10b40469169c146e3bd3a1883e4f05e
tergytrg <73442558+tergytrg@users.noreply.github.com>
2022-07-14 21:16:52 +0200

    Help aangepast
    Pakt nu automatisch beschrijving van ding.

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,17 @@
+const Init = require('./init');
+const New = require('./new');
+const View = require('./view');
+const Custom = require('./custom');
+const Remove = require('./remove');
+
 module.exports = {
     name: 'help',
     description: 'Lijst met commando\'s',
     execute(msg) {
-        const newInit = "\`!new\` : Maakt een nieuwe initiative.\n";
-        const init = "\`!init <bonus> <naam> <k1>\` : Maakt een initiative roll.\n";
-        const view = "\`!view\` : Stuurt de initiative lijst met nummertjes ervoor.\n";
-        const custom = "\`!custom <rol zonder bonus> <bonus> <naam>\` : Voeg handmatig een custom rol toe aan initiative.\n";
-        const remove = "\`!remove <positie>\` : Verwijder een rol van initiative.\n";
         msg.channel.send(
             "**__Speel commands:__**\n" + 
-            newInit + init + 
+            New.description + "\n" + Init.description + "\n" +
             "\n**__Admin commands:__** \n" +
-            view + custom + remove);
+            View.description + "\n" + Custom.description + "\n" + Remove.description);
     },
   };
